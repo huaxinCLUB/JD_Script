4bf7e107ffd37d403569f7f647dd863c74ba798a
wuzhi03 <82073070+wuzhi03@users.noreply.github.com>
2021-05-11 04:12:40 +0800

    cdn

--- jd_super_redrain.js
+++ jd_super_redrain.js
@@ -239,7 +239,7 @@ cron "1 0-23/1 * * *" script-path=https://raw.githubusercontent.com/nianyuguai/l
  }
  
  function rraUrl() {
-     let url = 'https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd-live-rain.json'
+     let url = 'https://cdn.jsdelivr.net/gh/nianyuguai/longzhuzhu@main/qx/jd-live-rain.json'
      if($.isNode() && process.env.JD_RRA_URL){
          url = process.env.JD_RRA_URL
      }else if($.getdata('jdRRAUrl')){
