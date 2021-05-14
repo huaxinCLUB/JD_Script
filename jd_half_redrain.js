4119970527fbe2220acc4c7aca55d128345a5549
wuzhi03 <82073070+wuzhi03@users.noreply.github.com>
2021-05-14 21:52:22 +0800

    去除github限制

--- jd_half_redrain.js
+++ jd_half_redrain.js
@@ -37,7 +37,6 @@ cron "30 20-23/1 * * *" script-path=https://raw.githubusercontent.com/nianyuguai
      })
      if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => {
      };
-     if (JSON.stringify(process.env).indexOf('GITHUB') > -1) process.exit(0)
  } else {
      cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
  }
