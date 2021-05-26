a7cc9636493d4ccc86d67d6ebad970ced585774b
wuzhi03 <82073070+wuzhi03@users.noreply.github.com>
2021-05-26 16:41:39 +0800

    增加环境变量JDZOOPK_SHARECODES，jd_zoo可做店铺活动

--- jd_city.js
+++ jd_city.js
@@ -251,7 +251,9 @@ function city_lotteryAward() {
 function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
-    $.get({url: `https://cdn.jsdelivr.net/gh/wuzhi-docker1/RandomShareCode@main/JD_City.json`, 'timeout': 10000}, (err, resp, data) => {
+    $.get({url: `https://gitee.com/linshi333/linshi/raw/master/city.json`,headers: {
+      "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
+  },'timeout': 10000}, (err, resp, data) => {
       try {
         if (err) {
           console.log(`${JSON.stringify(err)}`)
