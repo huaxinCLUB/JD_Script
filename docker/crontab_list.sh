26bbd3b2a70daf47a18ef40f6f7ec21f786b640e
wuzhi03 <82073070+wuzhi03@users.noreply.github.com>
2021-06-01 16:43:37 +0800

    移除过期，增加单独收金币js，调整动物corn

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -41,12 +41,11 @@
 #金榜创造营
 40 9,21 * * * node /scripts/jd_gold_creator.js >> /scripts/logs/jd_gold_creator.log 2>&1
 #动物联盟
-0 0-23/1 * * * node /scripts/jd_zoo.js >> /scripts/logs/jd_zoo.log 2>&1
+0 0,6-23/2 * * * node /scripts/jd_zoo.js >> /scripts/logs/jd_zoo.log 2>&1
+#动物联盟收集金币
+0-59/30 * * * * node /scripts/jd_zoo.js >> /scripts/logs/jd_zoo.log 2>&1
 #星推官
 0 0 * * * node /scripts/jd_xtg.js >> /scripts/logs/jd_xtg.log 2>&1
-
-#jd_party_night
-0 15-19/1 * * * node /scripts/jd_party_night.js >> /scripts/logs/jd_party_night.log 2>&1
 ##############长期活动##############
 # 签到
 0 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
