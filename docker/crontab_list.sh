9e1a3c4fe3a18f8bf12d4b17a5e15d3f21faa939
wuzhi03 <82073070+wuzhi03@users.noreply.github.com>
2021-06-03 21:48:38 +0800

    狂欢城系列

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -55,7 +55,11 @@
 #5g超级盲盒
 0 */4 * * * node /scripts/jd_mohe.js >> /scripts/logs/jd_mohe.log 2>&1
 #龙舟
-15 13 1-18 6 *node /scripts/jd_dragonboat.js >> /scripts/logs/jd_dragonboat.log 2>&1
+15 13 1-18 6 * node /scripts/jd_dragonboat.js >> /scripts/logs/jd_dragonboat.log 2>&1
+#品牌狂欢城
+15 9 1-18 6 * node /scripts/jd_brandcarnivalcity.js >> /scripts/logs/jd_brandcarnivalcity.log 2>&1
+#新潮品牌狂欢
+30 9,10 1-18 6 * node /scripts/jd_mcxhd_brandcity.js >> /scripts/logs/jd_mcxhd_brandcity.log 2>&1
 
 ##############长期活动##############
 # 签到
